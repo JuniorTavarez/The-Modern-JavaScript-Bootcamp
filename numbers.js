441ef9c0a73e3f4e0202ac1185ddc345e58ad1eb
Junior Tavarez <jtavarez8@gmail.com>
2020-02-13 14:19:24 -0500

    first upload for the course

---
+++ numbers.js
@@ -0,0 +1,14 @@
+let age = 26;
+let dogYear = (age + 1) / 7;
+
+console.log(dogYear);
+
+const studentScore = 18;
+
+const maxScore = 20;
+
+const percent = (studentScore, maxScore) => {
+  console.log((studentScore / maxScore) * 100);
+};
+
+percent(studentScore, maxScore);
---
+++ strings.js
@@ -0,0 +1,8 @@
+const city = 'Bronx';
+const country = 'USA';
+
+const location = (city, country) => {
+  console.log(city + ', ' + country);
+};
+
+location(city, country);
